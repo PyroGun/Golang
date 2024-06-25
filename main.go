df68a3f08a88aaca76a7f2d09ed558a3e10a4094
Aliaksandr Piranen <aliaksandr.piranen@ventionteams.com>
2024-06-25 15:27:41 +0300

    changed handler for /

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "I am working!")
+		fmt.Fprintf(w, "Server is working!")
 	})
 
 	log.Fatal(http.ListenAndServe(":80", nil))
